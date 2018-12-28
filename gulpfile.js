a18d614a73710bc6fce7631a320da0cfe9f9aff1
shen <2296239078@qq.com>
2018-12-28 20:05:15 +0800

    wancheng

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,10 @@ var gulp = require('gulp');
 var gulpscss = require('gulp-sass');
 var clean = require('gulp-clean-css')
 var uglify = require('gulp-uglify');
+var server = require("gulp-webserver")
+var url = require('url');
+var fs = require('fs');
+var path = require('path');
 gulp.task('aaaa', function() {
     gulp.src('./work/scss/*.scss')
         .pipe(gulpscss())
@@ -18,4 +22,24 @@ gulp.task('js', function() {
     gulp.src('./work/js/*.js')
         .pipe(uglify())
         .pipe(gulp.dest('./work/js'))
+})
+gulp.task('server', function() {
+    return gulp.src('work')
+        .pipe(server({
+            port: 8000,
+            open: true,
+            livereload: true,
+            middleware: function(req, res, next) {
+                var pathname = url.parse(req.url).pathname;
+                if (pathname == '/favicon.ico') {
+                    res.end('');
+                    return;
+                }
+                if (pathname == '/') {
+                    res.end(fs.readFileSync(path.join(__dirname, "work", "index.html")));
+                } else {
+                    res.end(fs.readFileSync(path.join(__dirname, "work", pathname)));
+                }
+            }
+        }))
 })
