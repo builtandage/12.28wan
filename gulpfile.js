2639af456086cdebbe353e7a57d93308a85a6edc
shen <2296239078@qq.com>
2018-12-28 19:38:07 +0800

    js压缩

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var gulpscss = require('gulp-sass');
 var clean = require('gulp-clean-css')
-
+var uglify = require('gulp-uglify');
 gulp.task('aaaa', function() {
     gulp.src('./work/scss/*.scss')
         .pipe(gulpscss())
@@ -10,4 +10,12 @@ gulp.task('aaaa', function() {
 })
 gulp.task('listen', function() {
     return gulp.watch('./work/scss/*.scss', gulp.series('aaaa'));
+})
+gulp.task('listen1', function() {
+    return gulp.watch('./work/js/*.js', gulp.series('js'));
+})
+gulp.task('js', function() {
+    gulp.src('./work/js/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./work/js'))
 })
